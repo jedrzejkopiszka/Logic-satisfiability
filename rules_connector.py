e9bfba8d45a245f099796ea2ab849ee04624f771
jedrzejkopiszka <62968948+jedrzejkopiszka@users.noreply.github.com>
2020-12-01 13:25:09 +0100

    Update rules_connector.py

--- rules_connector.py
+++ rules_connector.py
@@ -53,17 +53,3 @@ for i in new_general_tab:
 print(new_general_tab)
 
 print('Satisfiability: ', is_satisfiable(new_general_tab))
-
-
-# Sample testcases are listed below
-
-# A B C p/3 B p/1 C p/1 IMPLIES OR C p/1 E F p/2 E p/1 AND XOR OR NOT
-# X X p/1 Y p/1 AND FORALL
-# X X p/1 Y p/1 AND EXISTS NOT
-# C A B C p/3 B p/1 C p/1 IMPLIES OR C p/1 E F p/2 E p/1 A  ND XOR OR NOT FORALL
-# B A p/1 B p/1 C p/1 AND NOT IMPLIES EXISTS NOT
-# B A p/1 B p/1 IFF C p/1 D p/1 AND AND NOT EXISTS NOT
-# X X Y p/2 FORALL B p/1 C p/1 AND NOT IMPLIES NOT
-# X X p/1 Y Y p/1 C p/1 IMPLIES EXISTS OR NOT FORALL
-# Z Z q/1 FORALL X X p/1 X q/1 AND FORALL IMPLIES NOT
-# X Y X p/1 Y q/1 IMPLIES NOT EXISTS FORALL
